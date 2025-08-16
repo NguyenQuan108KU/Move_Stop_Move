920a700c750ff394d2ac2d6a6e36e65ac28faadc
Nguyen Duc Ngoc Quan <nguyenducngocquan2004@gmail.com>
2025-08-16 14:44:57 +0700

    B9

---
+++ Assets/Scripts_ZombieCity/CheckCollider.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckCollider : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Wall"))
+        {
+            SetObjectAlpha(other.gameObject, 0.3f); // mờ đi
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Wall"))
+        {
+            SetObjectAlpha(other.gameObject, 1f); // hiện lại
+        }
+    }
+
+    void SetObjectAlpha(GameObject obj, float alpha)
+    {
+        Renderer[] renderers = obj.GetComponentsInChildren<Renderer>();
+        foreach (Renderer r in renderers)
+        {
+            foreach (Material mat in r.materials)
+            {
+                if (mat.HasProperty("_Color"))
+                {
+                    Color c = mat.color;
+                    c.a = alpha;
+                    mat.color = c;
+                }
+            }
+        }
+    }
+}
